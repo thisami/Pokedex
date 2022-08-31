d0f69b3710d0ab58c2f2ab2ec4f518b17a21e7ae
Ruben Seber <rseber@ibsag.local>
2022-08-31 11:49:52 +0200

    Apex Chart Beispiel eingefügt

--- pokedex-app/src/app/app.component.ts
+++ pokedex-app/src/app/app.component.ts
@@ -21,13 +21,37 @@ export type ChartOptions = {
 
 export class AppComponent {
 
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions> | any;
 
 
   title = 'pokedex';
 
   public pokemon: Pokemon[] = [];
 
-  constructor(private http: HttpClient, private dialog: MatDialog) { }
+  constructor(private http: HttpClient, private dialog: MatDialog) {
+
+    this.chartOptions = {
+      series: [
+        {
+          name: "My-series",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "My First Angular Chart"
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+      }
+    }
+  }
+
+
 
   openDialog() {
     const dialogConfig = new MatDialogConfig();
